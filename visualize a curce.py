8d7ae3f22cfe787ec8d0120c7b6f3464fb4b0c99
XueqianDeng <denghokin@gmail.com>
2023-08-26 14:12:53 -0400

    n working

--- visualize a curce.py
+++ visualize a curce.py
@@ -4,16 +4,17 @@ import numpy as np
 # Create a window
 win = visual.Window(size=(800, 600), units='pix', fullscr=False)
 
-# Create points along a curve
-num_points = 100
-x_values = np.linspace(-200, 200, num_points)
-y_values = np.sin(x_values / 100) * 100  # Example curve equation
+# Create points for a half-circle curve
+num_points = 50
+angle_values = np.linspace(0, np.pi, num_points)  # Angles from 0 to pi
+x_values = np.cos(angle_values) * 100  # x = r * cos(angle)
+y_values = np.sin(angle_values) * 100  # y = r * sin(angle)
 
 # Create a polygon stimulus to connect the points
-curve = visual.ShapeStim(win, vertices=list(zip(x_values, y_values)), fillColor=None, lineColor='green')
+half_circle_curve = visual.ShapeStim(win, vertices=list(zip(x_values, y_values)), fillColor=None, lineColor='blue')
 
-# Draw the curve
-curve.draw()
+# Draw the half-circle curve
+half_circle_curve.draw()
 win.flip()
 
 # Wait for a key press to close the window
