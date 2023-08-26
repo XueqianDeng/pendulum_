1db53b38050db83dd0114664f04c6b7fd58bae12
XueqianDeng <denghokin@gmail.com>
2023-08-26 14:19:49 -0400

    it is actually a straight line

--- visualize a curce.py
+++ visualize a curce.py
@@ -4,17 +4,20 @@ import numpy as np
 # Create a window
 win = visual.Window(size=(800, 600), units='pix', fullscr=False)
 
-# Create points for a half-circle curve
+# Create points for a quarter-circle curve
 num_points = 50
-angle_values = np.linspace(0, np.pi / 4, num_points)  # Angles from 0 to pi
+angle_values = np.linspace(0, np.pi / 2, num_points)  # Angles from 0 to pi/2
 x_values = np.cos(angle_values) * 100  # x = r * cos(angle)
 y_values = np.sin(angle_values) * 100  # y = r * sin(angle)
 
-# Create a polygon stimulus to connect the points
-half_circle_curve = visual.ShapeStim(win, vertices=zip(x_values, y_values), fillColor=None, lineColor='blue')
+# Combine points to create the quarter-circle curve
+curve_points = list(zip(x_values, y_values))
 
-# Draw the half-circle curve
-half_circle_curve.draw()
+# Create a line stimulus to represent the quarter-circle curve
+quarter_circle_curve = visual.Line(win, start=curve_points[0], end=curve_points[-1], lineColor='red')
+
+# Draw the quarter-circle curve
+quarter_circle_curve.draw()
 win.flip()
 
 # Wait for a key press to close the window
