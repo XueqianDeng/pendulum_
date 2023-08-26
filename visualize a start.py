6be062c370362da9a847005cf9d5ca779eec8d8f
XueqianDeng <denghokin@gmail.com>
2023-08-26 14:36:09 -0400

    too hard for you

--- visualize a start.py
+++ visualize a start.py
@@ -1,29 +1,42 @@
 # Import necessary libraries
 from psychopy import visual, event, core
+import numpy as np
 
 # Create a window
 win = visual.Window(size=(800, 600), units='pix')
 
 # Define colors
-yellow = (1, 1, 0)
+yellow = (1, 1, 0)  # Yellow in RGB
 
-# Create a star shape using Polygon
-star_points = [
-    (0, 100),    # Top point
-    (21, 30),    # Top right point
-    (100, 30),   # Right point
-    (36, -12),   # Bottom right point
-    (57, -90),   # Bottom point
-    (0, -48),    # Bottom left point
-    (-57, -90),  # Bottom left point
-    (-36, -12),  # Bottom left point
-    (-100, 30),  # Left point
-    (-21, 30)    # Top left point
-]
-star = visual.Polygon(win, vertices=star_points, fillColor=yellow)
+# Create a Chinese flag-like star using ShapeStim with custom vertices
+large_star_radius = 150
+small_star_radius = 50
+angle_offset = np.pi / 10  # Offset angle to start drawing the star
 
-# Draw the star
-star.draw()
+large_star_vertices = []
+small_star_vertices = []
+
+for i in range(5):
+    angle = angle_offset + i * (2 * np.pi / 5)
+    x_large = large_star_radius * np.cos(angle)
+    y_large = large_star_radius * np.sin(angle)
+    large_star_vertices.append((x_large, y_large))
+
+    # Add small stars around the large star
+    small_star_angle = angle + np.pi / 5
+    for j in range(4):
+        small_angle = small_star_angle + j * (2 * np.pi / 5)
+        x_small = small_star_radius * np.cos(small_angle) + x_large
+        y_small = small_star_radius * np.sin(small_angle) + y_large
+        small_star_vertices.append((x_small, y_small))
+
+# Create ShapeStim objects for the large and small stars
+large_star = visual.ShapeStim(win, vertices=large_star_vertices, fillColor=yellow, lineColor=None)
+small_stars = visual.ShapeStim(win, vertices=small_star_vertices, fillColor=yellow, lineColor=None)
+
+# Draw the Chinese flag-like stars
+# large_star.draw()
+small_stars.draw()
 
 # Update the window
 win.flip()
