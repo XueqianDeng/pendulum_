4371da9ca647eb0178b4714bb4c3ed1a78c62d0a
XueqianDeng <denghokin@gmail.com>
2023-08-26 14:05:46 -0400

    bugs

--- visualize_a_circle.py
+++ visualize_a_circle.py
@@ -3,11 +3,11 @@ from psychopy import visual, event
 # Create a window
 win = visual.Window(size=(800, 600), units='pix', fullscr=False)
 
-# Create a circle stimulus
-circle = visual.Circle(win, radius=100, fillColor='red')
+# Create a half circle stimulus
+half_circle = visual.Circle(win, radius=100, fillColor='blue', start=0, end=180)
 
-# Draw the circle
-circle.draw()
+# Draw the half circle
+half_circle.draw()
 win.flip()
 
 # Wait for a key press to close the window
@@ -15,5 +15,3 @@ event.waitKeys()
 
 # Close the window
 win.close()
-
-core.quit()
